56125ae2902b1c984fa558d28b72f15d526fd890
Tvoi_config <kolliceplay220@gmail.com>
2023-10-13 18:09:38 +0300

    +forms +shop

--- main/models.py
+++ main/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+
+    CHOICE_TYPE = (
+        ('book', 'Книга'),
+        ('magazine', 'Журнал'),
+    )
+
+    name = models.CharField(max_length=50, verbose_name="Имя")
+    type = models.CharField(max_length=50, choices=CHOICE_TYPE, default='book', verbose_name="Тип")
+    character = models.TextField(max_length=100, verbose_name="Характеристика")
+    price = models.IntegerField(max_length=50, verbose_name="Цена")
+    image = models.ImageField(null=True, blank=True, upload_to="image/")
+
+    class Meta:
+        
+        verbose_name = 'товар'
+        verbose_name_plural = 'список товаров'
+        
+
+    def __str__(self):
+        return self.name
+    
+class record(models.Model):
+    name = models.CharField(max_length=150, verbose_name='ФИО')
+    number = models.CharField(max_length=150, verbose_name='Номер телефона')
+    message = models.CharField(max_length=200, verbose_name='Сообщение')
+    
+    class Meta:
+#        managed = False
+        db_table = 'record'
+        verbose_name = 'запись'
+        verbose_name_plural = 'Записи'
+        
+    def __str__(self):
+        return self.message
--- main/views.py
+++ main/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from .models import *
+from .forms import RecordForm
 
 def index(request):
     return render(request, 'main/index.html')
 
 def about(request):
-    return render(request, 'main/about.html')
+    if request.method == 'POST':
+        form = RecordForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = RecordForm()
+        
+    return render(request, 'main/about.html', {'form': form})
 
 def shop(request):
-    return render(request, 'main/shop.html')
-
+    product = Products.objects.all()
+    return render(request, 'main/shop.html', {'product': product})
